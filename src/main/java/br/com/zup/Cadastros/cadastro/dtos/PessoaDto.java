aa924800902653a32c28cb1dd0e1696b73ce367b
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-08 14:34:01 -0300

    Modelar classe pessoa dto

---
+++ src/main/java/br/com/zup/Cadastros/cadastro/dtos/PessoaDto.java
@@ -0,0 +1,103 @@
+package br.com.zup.Cadastros.cadastro.dtos;
+
+import javax.persistence.Column;
+
+public class PessoaDto {
+
+    private String cpf;
+    private String nome;
+    private String sobrenome;
+    private String cidade;
+    private String bairro;
+    private String nomeDoParenteProximo;
+    private boolean moraSozinho;
+    private boolean temPet;
+    private int idade;
+
+    public PessoaDto() {
+    }
+
+    public PessoaDto(String cpf, String nome, String sobrenome, String cidade, String bairro, String nomeDoParenteProximo, boolean moraSozinho, boolean temPet, int idade) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.nomeDoParenteProximo = nomeDoParenteProximo;
+        this.moraSozinho = moraSozinho;
+        this.temPet = temPet;
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNomeDoParenteProximo() {
+        return nomeDoParenteProximo;
+    }
+
+    public void setNomeDoParenteProximo(String nomeDoParenteProximo) {
+        this.nomeDoParenteProximo = nomeDoParenteProximo;
+    }
+
+    public boolean isMoraSozinho() {
+        return moraSozinho;
+    }
+
+    public void setMoraSozinho(boolean moraSozinho) {
+        this.moraSozinho = moraSozinho;
+    }
+
+    public boolean isTemPet() {
+        return temPet;
+    }
+
+    public void setTemPet(boolean temPet) {
+        this.temPet = temPet;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}

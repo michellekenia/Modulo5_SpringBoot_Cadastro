d7227271f0e16025a680793297c05e551e0556ad
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-08 21:00:31 -0300

    Criar lista de cadastro

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroRepository.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroRepository.java
@@ -2,5 +2,9 @@ package br.com.zup.Cadastros.cadastro;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CadastroRepository extends CrudRepository<Cadastro, String> {
+
+    public List <Cadastro> findAll ();
 }
